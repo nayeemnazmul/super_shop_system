5447b325d72e10d46e09d43ea307c14224691126
Nazmul Islam <md.nazmul@northsouth.edu>
2021-06-22 19:37:43 +0600

    fix createsuperuser from terminal args

--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # start-server.sh
-#if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-#    (cd super_shop_system; python manage.py createsuperuser --no-input)
-#fi
+
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
+    (python manage.py createsuperuser --no-input)
+fi
+
 (gunicorn super_shop_system.wsgi:application --user www-data --bind 0.0.0.0:8010 --workers 3) &
 nginx -g "daemon off;"
